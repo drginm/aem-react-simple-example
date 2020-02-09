afafe64878f24d4b7ad736a8811f63b6719e0e69
drginm <drginm>
2020-02-09 15:19:54 -0500

    Added configuration for the sling model to expose the properties

--- core/src/main/java/co/dlighthouse/aem/reactsimpleexample/core/models/BasicModel.java
+++ core/src/main/java/co/dlighthouse/aem/reactsimpleexample/core/models/BasicModel.java
@@ -7,9 +7,17 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+import org.apache.sling.api.SlingHttpServletRequest;
+import com.adobe.cq.export.json.ComponentExporter;
+import org.apache.sling.models.annotations.Exporter;
+import com.adobe.cq.export.json.ExporterConstants;
 
-@Model(adaptables = Resource.class)
-public class BasicModel {
+@Model(adaptables = SlingHttpServletRequest.class,
+        adapters = {ComponentExporter.class},
+        resourceType = BasicModel.BASIC_COMPONENT_RESOURCETYPE)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class BasicModel implements ComponentExporter {
+    protected static final String BASIC_COMPONENT_RESOURCETYPE = "react-simple-example/components/basic-component";
     @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
     protected String text;
     @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
@@ -20,5 +28,8 @@ public class BasicModel {
     }
     public Boolean isCheckbox() {
         return checkbox;
+    }
+    public String getExportedType() {
+        return BASIC_COMPONENT_RESOURCETYPE;
     }
 }
